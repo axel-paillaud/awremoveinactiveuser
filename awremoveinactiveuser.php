8f0bedb92856d1dbeb03cbda0ca803aee553c5b9
Axel Paillaud <contact@axelweb.fr>
2025-12-16 13:50:33 +0100

    first commit, init base module

---
+++ awremoveinactiveuser.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * 2007-2024 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Axelweb <contact@axelweb.fr>
+ * @copyright 2007-2024 Axelweb
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of Axelweb
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+class AwModuleBase extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'awremoveinactiveuser';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Axelweb';
+        $this->need_instance = 0;
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->trans('Remove Inactive User', [], 'Modules.Awremoveinactiveuser.Admin');
+        $this->description = $this->trans('PrestaShop CLI module for deleting inactive customers to comply with GDPR.', [], 'Modules.Awremoveinactiveuser.Admin');
+
+        $this->confirmUninstall = $this->trans('Are you sure you want to uninstall this module?', [], 'Modules.Awremoveinactiveuser.Admin');
+
+        $this->ps_versions_compliancy = [
+            'min' => '8.0',
+            'max' => _PS_VERSION_,
+        ];
+    }
+
+    public function isUsingNewTranslationSystem()
+    {
+        return true;
+    }
+
+    public function install(): bool
+    {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+
+        $installed = parent::install();
+
+        // Prevent 'Unable to generate a URL for the named route [...]' error,
+        // clear Symfony cache
+        if ($installed) {
+            Tools::clearSf2Cache();
+        }
+
+        return $installed;
+    }
+
+    public function uninstall(): bool
+    {
+        return parent::uninstall();
+    }
+}
